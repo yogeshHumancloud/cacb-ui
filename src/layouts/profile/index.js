dce253b90f4aecdc528e60ce1e92b110ab6b650a
aaruu0111 <aarti.shegar@humancloud.co.in>
2023-11-10 11:39:05 +0530

    profile changes

--- src/layouts/profile/index.js
+++ src/layouts/profile/index.js
@@ -13,190 +13,166 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
-// @mui material components
-import Grid from "@mui/material/Grid";
-import Divider from "@mui/material/Divider";
+// @mui material componentsimport React from 'react';
 
-// @mui icons
-import FacebookIcon from "@mui/icons-material/Facebook";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import InstagramIcon from "@mui/icons-material/Instagram";
-
-// Material Dashboard 2 React components
-import MDBox from "components/MDBox";
-import MDTypography from "components/MDTypography";
-
-// Material Dashboard 2 React example components
-import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
-import DashboardNavbar from "examples/Navbars/DashboardNavbar";
-import Footer from "examples/Footer";
-import ProfileInfoCard from "examples/Cards/InfoCards/ProfileInfoCard";
-import ProfilesList from "examples/Lists/ProfilesList";
-import DefaultProjectCard from "examples/Cards/ProjectCards/DefaultProjectCard";
-
-// Overview page components
-import Header from "layouts/profile/components/Header";
-import PlatformSettings from "layouts/profile/components/PlatformSettings";
-
-// Data
-import profilesListData from "layouts/profile/data/profilesListData";
-
-// Images
-import homeDecor1 from "assets/images/home-decor-1.jpg";
-import homeDecor2 from "assets/images/home-decor-2.jpg";
-import homeDecor3 from "assets/images/home-decor-3.jpg";
-import homeDecor4 from "assets/images/home-decor-4.jpeg";
-import team1 from "assets/images/team-1.jpg";
-import team2 from "assets/images/team-2.jpg";
-import team3 from "assets/images/team-3.jpg";
-import team4 from "assets/images/team-4.jpg";
+import React, { useState } from 'react';
+import {
+  Paper,
+  Typography,
+  Grid,
+  Divider,
+  IconButton,
+  TextField,
+  Checkbox,
+  FormControlLabel,
+  Select,
+  MenuItem,
+  Button,
+} from '@material-ui/core';
+import EditIcon from '@material-ui/icons/Edit';
 
 function Overview() {
+  const [editing, setEditing] = useState(false);
+  const [fullName, setFullName] = useState('John Doe');
+  const [shippingAddress, setShippingAddress] = useState('');
+  const [billingAddress, setBillingAddress] = useState('');
+  const [hasBillingAddress, setHasBillingAddress] = useState(true);
+  const [organization, setOrganization] = useState('');
+  const [email, setEmail] = useState('john.doe@example.com');
+  const [mobilePhone, setMobilePhone] = useState('');
+  const [defaultCurrency, setDefaultCurrency] = useState('INR');
+
+  const handleEditClick = () => {
+    setEditing(true);
+  };
+
+  const handleSaveClick = () => {
+    console.log({
+      fullName,
+      shippingAddress,
+      billingAddress,
+      hasBillingAddress,
+      organization,
+      email,
+      mobilePhone,
+      defaultCurrency,
+    });
+    setEditing(false);
+  };
+  const rows = [
+    { id: 1, name: 'Full Name', value: fullName, fontWeight: 'bold' },
+    { id: 2, name: 'Add Address', fontWeight: 'bold' },
+    { id: 3, name: 'Organization', value: organization, fontWeight: 'bold' },
+    { id: 4, name: 'Email', value: email, fontWeight: 'bold' },
+    { id: 5, name: 'Mobile Phone', value: mobilePhone, fontWeight: 'bold' },
+    { id: 6, name: 'Default currency', value: defaultCurrency, fontWeight: 'bold' },
+  ];
+
   return (
-    <DashboardLayout>
-      <DashboardNavbar />
-      <MDBox mb={2} />
-      <Header>
-        <MDBox mt={5} mb={3}>
-          <Grid container spacing={1}>
-            <Grid item xs={12} md={6} xl={4}>
-              <PlatformSettings />
-            </Grid>
-            <Grid item xs={12} md={6} xl={4} sx={{ display: "flex" }}>
-              <Divider orientation="vertical" sx={{ ml: -2, mr: 1 }} />
-              <ProfileInfoCard
-                title="profile information"
-                description="Hi, I’m Alec Thompson, Decisions: If you can’t decide, the answer is no. If two equally difficult paths, choose the one more painful in the short term (pain avoidance is creating an illusion of equality)."
-                info={{
-                  fullName: "Alec M. Thompson",
-                  mobile: "(44) 123 1234 123",
-                  email: "alecthompson@mail.com",
-                  location: "USA",
-                }}
-                social={[
-                  {
-                    link: "https://www.facebook.com/CreativeTim/",
-                    icon: <FacebookIcon />,
-                    color: "facebook",
-                  },
-                  {
-                    link: "https://twitter.com/creativetim",
-                    icon: <TwitterIcon />,
-                    color: "twitter",
-                  },
-                  {
-                    link: "https://www.instagram.com/creativetimofficial/",
-                    icon: <InstagramIcon />,
-                    color: "instagram",
-                  },
-                ]}
-                action={{ route: "", tooltip: "Edit Profile" }}
-                shadow={false}
-              />
-              <Divider orientation="vertical" sx={{ mx: 0 }} />
-            </Grid>
-            <Grid item xs={12} xl={4}>
-              <ProfilesList title="conversations" profiles={profilesListData} shadow={false} />
-            </Grid>
+    <>
+      <div style={{ display: 'flex', alignItems: 'center', marginLeft: '300px', marginTop: '75px' }}>
+        <Typography variant="v5" style={{ height: 90, width: 120, fontWeight: 'bold', textDecoration: 'underline' }}> MY PROFILE</Typography>
+        <IconButton aria-label="edit" style={{ marginLeft: '1200px' }} onClick={handleEditClick}>
+          <EditIcon />
+        </IconButton>
+      </div>
+      <div style={{ marginLeft: '300px', marginTop: '20px', marginRight: '70px' }}>
+        <Paper>
+          <Grid container spacing={2} style={{ padding: '20px' }}>
+            {rows.map((row) => (
+              <React.Fragment key={row.id}>
+                <Grid container item xs={12} style={{ paddingBottom: '0', fontSize: '18px' }}>
+                  <Grid item xs={10}>
+                    <div style={{ fontWeight: row.fontWeight }}>{row.name}</div>
+                  </Grid>
+                </Grid>
+                {row.name === 'Full Name' && editing && (
+                  <Grid item xs={12} style={{ paddingTop: '0', fontSize: '15px' }}>
+                    <TextField
+                      value={fullName}
+                      onChange={(e) => setFullName(e.target.value)}
+                      label="Full Name"
+                    />
+                  </Grid>
+                )}
+                {row.name === 'Add Address' && editing && (
+                  <Grid item xs={12} style={{ paddingTop: '0', fontSize: '15px' }}>
+                    <TextField
+                      value={shippingAddress}
+                      onChange={(e) => setShippingAddress(e.target.value)}
+                      label="Shipping Address"
+                    />
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={hasBillingAddress}
+                          onChange={() => setHasBillingAddress(!hasBillingAddress)}
+                        />
+                      }
+                      label="Use same address for Billing"
+                    />
+                    {hasBillingAddress && (
+                      <TextField
+                        value={billingAddress}
+                        onChange={(e) => setBillingAddress(e.target.value)}
+                        label="Billing Address"
+                      />
+                    )}
+                  </Grid>
+                )}
+                {row.name === 'Organization' && editing && (
+                  <Grid item xs={12} style={{ paddingTop: '0', fontSize: '15px' }}>
+                    <TextField
+                      value={organization}
+                      onChange={(e) => setOrganization(e.target.value)}
+                      label="Organization"
+                    />
+                  </Grid>
+                )}
+                {row.name === 'Email' && (
+                  <Grid item xs={12} style={{ paddingTop: '0', fontSize: '15px' }}>
+                    <TextField
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      disabled
+                    />
+                  </Grid>
+                )}
+                {row.name === 'Mobile Phone' && editing && (
+                  <Grid item xs={12} style={{ paddingTop: '0', fontSize: '15px' }}>
+                    <TextField
+                      value={mobilePhone}
+                      onChange={(e) => setMobilePhone(e.target.value)}
+                      label="Mobile Phone"
+                    />
+                  </Grid>
+                )}
+                {row.name === 'Default currency' && editing && (
+                  <Grid item xs={12} style={{ paddingTop: '0', fontSize: '15px' }}>
+                    <Select
+                      value={defaultCurrency}
+                      onChange={(e) => setDefaultCurrency(e.target.value)}
+                    >
+                      <MenuItem value="INR">INR</MenuItem>
+                    </Select>
+                  </Grid>
+                )}
+                <Grid item xs={12}>
+                  <Divider />
+                </Grid>
+              </React.Fragment>
+            ))}
           </Grid>
-        </MDBox>
-        <MDBox pt={2} px={2} lineHeight={1.25}>
-          <MDTypography variant="h6" fontWeight="medium">
-            Projects
-          </MDTypography>
-          <MDBox mb={1}>
-            <MDTypography variant="button" color="text">
-              Architects design houses
-            </MDTypography>
-          </MDBox>
-        </MDBox>
-        <MDBox p={2}>
-          <Grid container spacing={6}>
-            <Grid item xs={12} md={6} xl={3}>
-              <DefaultProjectCard
-                image={homeDecor1}
-                label="project #2"
-                title="modern"
-                description="As Uber works through a huge amount of internal management turmoil."
-                action={{
-                  type: "internal",
-                  route: "/pages/profile/profile-overview",
-                  color: "info",
-                  label: "view project",
-                }}
-                authors={[
-                  { image: team1, name: "Elena Morison" },
-                  { image: team2, name: "Ryan Milly" },
-                  { image: team3, name: "Nick Daniel" },
-                  { image: team4, name: "Peterson" },
-                ]}
-              />
-            </Grid>
-            <Grid item xs={12} md={6} xl={3}>
-              <DefaultProjectCard
-                image={homeDecor2}
-                label="project #1"
-                title="scandinavian"
-                description="Music is something that everyone has their own specific opinion about."
-                action={{
-                  type: "internal",
-                  route: "/pages/profile/profile-overview",
-                  color: "info",
-                  label: "view project",
-                }}
-                authors={[
-                  { image: team3, name: "Nick Daniel" },
-                  { image: team4, name: "Peterson" },
-                  { image: team1, name: "Elena Morison" },
-                  { image: team2, name: "Ryan Milly" },
-                ]}
-              />
-            </Grid>
-            <Grid item xs={12} md={6} xl={3}>
-              <DefaultProjectCard
-                image={homeDecor3}
-                label="project #3"
-                title="minimalist"
-                description="Different people have different taste, and various types of music."
-                action={{
-                  type: "internal",
-                  route: "/pages/profile/profile-overview",
-                  color: "info",
-                  label: "view project",
-                }}
-                authors={[
-                  { image: team4, name: "Peterson" },
-                  { image: team3, name: "Nick Daniel" },
-                  { image: team2, name: "Ryan Milly" },
-                  { image: team1, name: "Elena Morison" },
-                ]}
-              />
-            </Grid>
-            <Grid item xs={12} md={6} xl={3}>
-              <DefaultProjectCard
-                image={homeDecor4}
-                label="project #4"
-                title="gothic"
-                description="Why would anyone pick blue over pink? Pink is obviously a better color."
-                action={{
-                  type: "internal",
-                  route: "/pages/profile/profile-overview",
-                  color: "info",
-                  label: "view project",
-                }}
-                authors={[
-                  { image: team4, name: "Peterson" },
-                  { image: team3, name: "Nick Daniel" },
-                  { image: team2, name: "Ryan Milly" },
-                  { image: team1, name: "Elena Morison" },
-                ]}
-              />
-            </Grid>
-          </Grid>
-        </MDBox>
-      </Header>
-      <Footer />
-    </DashboardLayout>
+        </Paper>
+        {editing && (
+          <div style={{ textAlign: 'center', marginTop: '20px' }}>
+            <Button variant="contained" color="primary" onClick={handleSaveClick}>
+              Save
+            </Button>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
